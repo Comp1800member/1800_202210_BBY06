48562fe4876a923a9e212ec7408c5faa97c7a53d
Lester Shun <lester.shunnn@gmail.com>
2022-03-02 12:27:52 -0800

    login screen with firebase

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAGo_ZOgTSn2uy62qel_m595fXmXiNawfA",
+  authDomain: "bby06-attendr.firebaseapp.com",
+  projectId: "bby06-attendr",
+  storageBucket: "bby06-attendr.appspot.com",
+  messagingSenderId: "884971187466",
+  appId: "1:884971187466:web:43326a132d39becfbb1ef3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
