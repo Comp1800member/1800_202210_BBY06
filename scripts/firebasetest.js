2e5d63937e9f3ca24eff1d7eb47b12efb22889db
Bucksin <bucksin8@gmail.com>
2022-03-08 13:01:55 -0800

    added firebase test code for database testing

---
+++ scripts/firebasetest.js
@@ -0,0 +1,40 @@
+
+let uploadButton = document.getElementById("upload-button");
+let downloadButton = document.getElementById("download-button");
+
+uploadButton.addEventListener("click", ()=> {
+    
+    db.collection("users").doc("testUser").set({
+          name: "hello this is a test",
+          email: "again, a test"
+          
+        }).then(function(){
+          console.log("new info added to firestore");
+          //window.localStorage.assign("main.html");
+        })
+        .catch(function(error){
+          console.log(error);
+        })
+    
+    //adds a test attendee in the attendeeList in the testUser collection
+    db.collection("users").doc("testUser").collection("attendeeList").doc("testAttendee1").set({})
+    .then(function () {
+        console.log("new collection added to firestore.");
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+
+    //adds a test event in the eventList in the testUser collection
+    db.collection("users").doc("testUser").collection("eventList").doc("testEvent1").set({})
+    .then(function () {
+        console.log("new collection added to firestore.");
+    })
+    .catch(function (error) {
+        console.log(error);
+    })
+})
+
+downloadButton.addEventListener("click", ()=> {
+
+})
