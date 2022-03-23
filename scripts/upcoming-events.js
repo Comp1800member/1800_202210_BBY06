54535c567a0a40bd86c6c549fd7dd85fc17eae65
Bucksin <bucksin8@gmail.com>
2022-03-23 13:30:04 -0700

    edited upcoming events page

--- scripts/upcoming-events.js
+++ scripts/upcoming-events.js
@@ -1,20 +1,27 @@
+//window selectors//
+let eventWindowOne = document.getElementById("window-one");
+let eventWindowTwo = document.getElementById("window-two");
+let eventWindowThree = document.getElementById("window-three");
+
+
 
 //load event list function
 
 let today = new Date(3 , 1, 2022);
 
-loadListButton.addEventListener("click", () => {
+loadListButton.addEventListener("click", loadUpcomingEvents);
 
+//loads upcoming events on the page//
+function loadUpcomingEvents () {
 
     eventList = db.collection("users").doc("testUser").collection("eventList");
     eventList
-        .where("dateTime", ">", today )
+        .where("dateTime", ">=", today )
         .orderBy("dateTime")
         .get()
         .then(userDoc => {
             let upcomingEventTemplate = document.getElementById("upcoming-event-bar-template");
             let eventTemplate = document.getElementById("event-bar-template");
-            let eventWindowOne = document.getElementById("window-one");
 
 
             for (i = 0; i < Number(userDoc.docs.length); i++) {
@@ -26,25 +33,41 @@ loadListButton.addEventListener("click", () => {
                     let newEventBar = upcomingEventTemplate.content.cloneNode(true);
                     newEventBar.getElementById("event-title").innerHTML = eventData.name;
                     newEventBar.getElementById("event-date").innerHTML = displayDate(eventData['dateTime'].toDate());
+                    newEventBar.getElementById("event-time").innerHTML = eventData.dateTime.toDate().toLocaleTimeString();
                     newEventBar.getElementById("event-description").innerHTML = eventData.description;
                     newEventBar.getElementById("event-capacity").innerHTML = eventData.capacity;
-                    //console.log(eventData.name);
-                    //console.log(eventData.dateTime);
+                    newEventBar.querySelector(".upcoming-event-bar").onclick = () => console.log("hello");
+                    let nextEventLabel = document.createElement("h4");
+                    nextEventLabel.innerHTML = "Next Event";
+                    nextEventLabel.classList.add("list-divider");
+                    eventWindowOne.appendChild(nextEventLabel);
+
                     eventWindowOne.appendChild(newEventBar);
+
+                    let upcomingEventLabel = document.createElement("h4");
+                    upcomingEventLabel.innerHTML = "Upcoming Events";
+                    upcomingEventLabel.classList.add("list-divider");
+                    eventWindowOne.appendChild(upcomingEventLabel);
 
                 } else {
                     let newEventBar = eventTemplate.content.cloneNode(true);
                     newEventBar.getElementById("event-title").innerHTML = eventData.name;
                     newEventBar.getElementById("event-date").innerHTML = displayDate(eventData['dateTime'].toDate());
-                    newEventBar.getElementById("event-time").innerHTML = eventData.time;
+                    newEventBar.getElementById("event-time").innerHTML = eventData.dateTime.toDate().toLocaleTimeString();
                     newEventBar.getElementById("event-description").innerHTML = eventData.description;
+                    newEventBar.querySelector(".event-bar").onclick = () => console.log("hello");
                     eventWindowOne.appendChild(newEventBar);
 
                 }
 
             }
+
         })
-})
+    }
+
+function loadEventDetails () {
+
+}
 
 
 function displayDate (date) {
