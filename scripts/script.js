500e8bccc50ad78867e47c382a3c91679460f251
Comp1800member <Jonathanliu865@gmail.com>
2022-03-23 11:47:46 -0700

    message

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,7 @@ let pageOneButton = document.getElementById("page-one-button");
 let pageTwoButton = document.getElementById("page-two-button");
 let pageThreeButton = document.getElementById("page-three-button");
 let contentWindows = document.getElementById("content-windows");
+//let upcomingEventButton = document.getElementById("upcoming-events-button")
 
 //event listeners for pop in menus
 hamburgerButton.addEventListener("click", () => {
@@ -81,3 +82,49 @@ loadListButton.addEventListener("click", () => {
             }
         })
 })
+
+/*loadUpcomingEventButton.addEventListener("click", () => {
+
+
+    upcomingEventList = db.collection("users").doc("testUser").collection("eventList");
+    upcomingEventList.get()
+        .then(userDoc => {
+            let upcomingEventTemplate = document.getElementById("upcoming-event-bar-template");
+            let eventTemplate = document.getElementById("event-bar-template");
+            let eventWindowOne = document.getElementById("window-one");
+            console.log(userDoc.docs);
+            console.log("length: " + userDoc.docs.length);
+            console.log("userDoc.docs[1]: " + JSON.stringify(userDoc.docs[1]));
+            console.log(userDoc.docs[1].id);
+            console.log(userDoc.docs[1].data().name);
+                        for (i = 0; i < Number(userDoc.docs.length); i++) {
+                let eventData = userDoc.docs[i].data();
+
+                console.log("eventData: " + eventData);
+
+                if (i === 0) {
+                    let newEventBar = upcomingEventTemplate.content.cloneNode(true);
+                    newEventBar.getElementById("event-title").innerHTML = eventData.name;
+                    newEventBar.getElementById("event-date").innerHTML = eventData.date;
+                    newEventBar.getElementById("event-time").innerHTML = eventData.time;
+                    newEventBar.getElementById("event-description").innerHTML = eventData.description;
+                    newEventBar.getElementById("event-capacity").innerHTML = eventData.capacity;
+                    
+                    eventWindowOne.appendChild(newEventBar);
+
+                } else {
+                    let newEventBar = eventTemplate.content.cloneNode(true);
+                    newEventBar.getElementById("event-title").innerHTML = eventData.name;
+                    newEventBar.getElementById("event-date").innerHTML = eventData.date;
+                    newEventBar.getElementById("event-time").innerHTML = eventData.time;
+                    newEventBar.getElementById("event-description").innerHTML = eventData.description;
+
+                    eventWindowOne.appendChild(newEventBar);
+
+                }
+
+            }
+        })
+})
+
+*/
