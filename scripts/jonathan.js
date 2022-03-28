68a25d5786c69ebea2e23bf4f7a46fe1ed8d558a
Comp1800member <Jonathanliu865@gmail.com>
2022-03-28 14:30:09 -0700

    Completed the add and delete event function

--- scripts/jonathan.js
+++ scripts/jonathan.js
@@ -4,42 +4,121 @@
 //created input field and button in main.html
 
 //delete event button
-let addEventName = document.getElementById("add-event-button");//linked to add-event-button in main
-let eventDeletion = document.getElementById("delete-event-button");//linked to newly created delete-event-button
+let addEventSubmitButton = document.getElementById("add-event-submit-button"); //linked to add-event-button in main
+let eventToDelete;
+let deleteEventButton = document.getElementById("delete-event-button"); //linked to newly created delete-event-button
+let inputEventName;
+let inputEventDate;
+let inputEventTime;
+let inputEventCapacity;
+let inputEventDescription;
+let inputEventGroup;
 
-function deleteEvent (eventDeletion) {
-    //get input parameters and use those as the string for the event to delete.
-    //do the same thing for the add event button
-    //let userName = document.getElementById("name-input").value
-
-    console.log("Testing event deletion function");
-  
-    db.collection("users").doc("testUser").collection("eventList").doc("Buck").delete()
-    // console.log("after delete");
+//sets the event to delete variable
+function deleteSetter() {
+  if (document.getElementById("delete-event-input") != null) {
+    eventToDelete = document.getElementById("delete-event-input").value;
+  } else {
+    console.log("Name of event to delete must not be null");
+  }
 }
 
-eventDeletion.addEventListener("click", () => {
-    console.log("This is delete button");
-    deleteEvent("dummyEvent2");
-})
 
-function addEvent (addEventName) {
-  db.collection("users").doc("testUser").collection("eventList").doc("Buck").set({/* Will eventually need to link addEventName(above) to
-  input fields in the add Event form and also add details into the document fields with the input form*/
-        
-  }).then(function(){
-    console.log("new info added to firestore");
-    //window.localStorage.assign("main.html");
-  })
-  .catch(function(error){
-    console.log(error);
-  })
+// sets all of the input fields to a variable if they are inputted
+function setter() {
+  if (document.getElementById("event-form-input-name") != null) {
+    inputEventName = document.getElementById("event-form-input-name").value;
+  } else {
+    console.log("Event name input must not be null");
+  }
+
+  if (document.getElementById("event-form-input-date") != null) {
+    inputEventDate = document.getElementById("event-form-input-date").value;
+  } else {
+    console.log("Event date input must not be null");
+  }
+
+  if (document.getElementById("event-form-input-time") != null) {
+    inputEventTime = document.getElementById("event-form-input-time").value;
+  } else {
+    console.log("Event time input must not be null");
+  }
+
+  if (document.getElementById("event-form-input-capacity") != null) {
+    inputEventCapacity = document.getElementById("event-form-input-capacity").value;
+  } else {
+    console.log("Event capacity input must not be null");
+  }
+
+  if (document.getElementById("event-form-input-description") != null) {
+    inputEventDescription = document.getElementById("event-form-input-description").value;
+  } else {
+    console.log("Event description input must not be null");
+    alert("description must not be empty");
+  }
+
+  if (document.getElementById("event-form-input-group") != null) {
+    inputEventGroup = document.getElementById("event-form-input-group").value;
+  }
 }
 
-addEventName.addEventListener("click", () => {//event listener for add-event-button
-  console.log("This is add event button");
-  addEvent(addEventName);
-})
+function addEvent(inputEventName) {
+  db.collection("users").doc("testUser").collection("eventList").doc(inputEventName).set({
+      /* Will eventually need to link addEventName(above) to
+       input fields in the add Event form and also add details into the document fields with the input form*/
+      Name: inputEventName,
+      Date: inputEventDate,
+      Time: inputEventTime,
+      Capacity: inputEventCapacity,
+      Description: inputEventDescription,
+      Group: inputEventGroup
+    }).then(function () {
+      console.log("new info added to firestore");
+      //window.localStorage.assign("main.html");
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+}
 
+addEventSubmitButton.addEventListener("click", () => {
+  console.log("This is add-event button");
+  setter();
+  if (inputEventName == "" || inputEventDate == "" || inputEventCapacity == "" || inputEventDescription == ""
+  || inputEventTime == "") {
+    alert("Event name, date, capacity, description, time, and group must not be empty!");
+  }
+  addEvent(inputEventName);
+})
 //create event button
 
+//delete event stuff down here
+//need to link
+function deleteEvent(eventToDelete) {
+  //get input parameters and use those as the string for the event to delete.
+  //do the same thing for the add event button
+  //let userName = document.getElementById("name-input").value
+
+  console.log("Testing event deletion function");
+  eventToDelete.toString();
+  db.collection("users").doc("testUser").collection("eventList").doc(eventToDelete).delete({
+    /* Will eventually need to link addEventName(above) to
+     input fields in the add Event form and also add details into the document fields with the input form*/
+  }).then(function () {
+    console.log("event deleted from firestore");
+    //window.localStorage.assign("main.html");
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  // console.log("after delete");
+}
+
+deleteEventButton.addEventListener("click", () => {
+  console.log("This is delete button");
+  deleteSetter();
+  if (eventToDelete == ""){
+    alert("The name of the event that you want to delete must not be empty");
+  }
+  deleteEvent(eventToDelete);
+})
