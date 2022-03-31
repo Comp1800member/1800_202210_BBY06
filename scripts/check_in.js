e7c6e96e114b25f187bf07b860acc76c3157b576
Lester Shun <lester.shunnn@gmail.com>
2022-03-31 03:33:44 -0700

    reorganized code in check_in.js

--- scripts/check_in.js
+++ scripts/check_in.js
@@ -1,41 +1,33 @@
+// gets params from the url itself
 let params = new URL(window.location.href);
 let userId = params.searchParams.get("userId");
 let eventId = params.searchParams.get("eventId");
 
+// elements and buttons from check_in.html
 let eventTitle = document.getElementById("event-title");
 let eventDescription = document.getElementById("event-description");
 let checkInButton = document.getElementById("check-in-button");
 let checkInForm = document.getElementById("check-in-form");
 let checkInSubmit = document.getElementById("check-in-submit-button");
 
+// the current user (organizer) and current event the attendee is checking in for
 let currentUserAndEvent = db.collection("users").doc(userId).collection("eventList").doc(eventId);
 
-currentUserAndEvent.get()
-    .then(result => {
-        console.log("The event title is", result.data().name);
-        eventTitle.innerHTML = result.data().name;
-        console.log("The event description is ", result.data().description);
-        eventDescription.innerHTML = result.data().description;
-    })
 
-hideCheckInForm();
-
-checkInButton.addEventListener("click", () => {
-    showCheckInForm(eventId);
-});
+// =========FUNCTION DEFINITIONS=========
 
 function hideCheckInForm() {
     checkInForm.hidden = true;
 }
 
-function showCheckInForm(eId) {
+function showCheckInForm() {
     checkInForm.hidden = false;
     checkInSubmit.addEventListener("click", () => {
-        checkIn(eId);
+        checkIn();
     });
 }
 
-function checkIn(docID) {
+function checkIn() {
 
     let attendeeFirstName = document.getElementById("attendee-fname").value;
     let attendeeLastName = document.getElementById("attendee-lname").value;
@@ -52,3 +44,22 @@ function checkIn(docID) {
         });
     }
 }
+
+// =========FUNCTION CALLS=========
+
+// loads event info into .event-bar
+currentUserAndEvent.get()
+    .then(result => {
+        console.log("The event title is", result.data().name);
+        eventTitle.innerHTML = result.data().name;
+        console.log("The event description is ", result.data().description);
+        eventDescription.innerHTML = result.data().description;
+    });
+
+// hides check in form on first load
+hideCheckInForm();
+
+// will give the giant check in button an event listener that starts the process to check in when clicked 
+checkInButton.addEventListener("click", () => {
+    showCheckInForm();
+});
